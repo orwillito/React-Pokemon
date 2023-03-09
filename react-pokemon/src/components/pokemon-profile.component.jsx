8fcc99941253e2286b9cce744d2b98ccc01e598a
Dick Orwille Mercado <orwille@gmail.com>
2023-03-09 10:28:21 +0800

    styled

---
+++ react-pokemon/src/components/pokemon-profile.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { PokemonProfileStyle } from './pokemon-card.styled'
+
+
+export default function PokemonProfile() {
+  return (
+    <PokemonProfileStyle>
+        <p>Something</p>
+    </PokemonProfileStyle>
+  )
+}
