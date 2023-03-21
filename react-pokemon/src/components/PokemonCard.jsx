27091fcdc310640349aba33948316cb35f86471d
Kirk Garcia <janngarcia24@gmail.com>
2023-03-21 09:03:19 +0800

    added-react-spinners

--- react-pokemon/src/components/PokemonCard.jsx
+++ react-pokemon/src/components/PokemonCard.jsx
@@ -4,14 +4,23 @@ import {
   PokemonCardStyle,
   Avatar,
   TypeClassStyle,
+  LoadingStyle,
 } from "../utils/ComponentsStylesheet";
+import CircleLoader from "react-spinners/CircleLoader";
 
 const PokemonCard = ({ pokemon, setPokeProfile }) => {
   const [pokemonInfos, setPokemonInfos] = useState();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     getPokemonIds(pokemon.name)
-      .then((data) => setPokemonInfos(data))
+      .then((data) => {
+        setPokemonInfos(data);
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 2000);
+      })
       .catch((err) => console.log("error", err));
   }, [pokemon]);
 
@@ -23,19 +32,33 @@ const PokemonCard = ({ pokemon, setPokeProfile }) => {
 
   return (
     <>
-      <PokemonCardStyle key={pokemon.name} onClick={handleClick}>
-        <Avatar
-          src={pokemonInfos?.sprites.other.dream_world.front_default}
-          alt="PokeImage"
-        />
+      {isLoading ? (
+        <LoadingStyle>
+          <CircleLoader
+            color={"#dc0a2d"}
+            loading={isLoading}
+            size={100}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </LoadingStyle>
+      ) : (
+        <>
+          <PokemonCardStyle key={pokemon.name} onClick={handleClick}>
+            <Avatar
+              src={pokemonInfos?.sprites.other.dream_world.front_default}
+              alt="PokeImage"
+            />
 
-        <h3>No. {pokemonInfos?.id}</h3>
-        <h2>{pokemon.name}</h2>
+            <h3>No. {pokemonInfos?.id}</h3>
+            <h2>{pokemon.name}</h2>
 
-        <TypeClassStyle type={pokemonInfos?.types[0].type.name}>
-          <h4>{pokemonInfos?.types[0].type.name}</h4>
-        </TypeClassStyle>
-      </PokemonCardStyle>
+            <TypeClassStyle type={pokemonInfos?.types[0].type.name}>
+              <h4>{pokemonInfos?.types[0].type.name}</h4>
+            </TypeClassStyle>
+          </PokemonCardStyle>
+        </>
+      )}
     </>
   );
 };
