2cc85cff1911b43950d97251b4b9f92209f49139
Dick Orwille Mercado <orwille@gmail.com>
2023-04-28 09:59:55 +0800

    for deployment

--- react-pokemon/src/App.js
+++ react-pokemon/src/App.js
@@ -1,23 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
-import Devs from './pages/Devs';
-import Home from './pages/Home';
-import Navigation from './pages/Navigation';
-
-
+import { Routes, Route } from "react-router-dom";
+import Devs from "./pages/Devs";
+import Home from "./pages/Home";
+import Navigation from "./pages/Navigation";
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Navigation />}>
+      <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='Devs' element={<Devs />} />
+        <Route path="devs" element={<Devs />} />
       </Route>
-
-      
     </Routes>
-  )
-}
-
-
+  );
+};
 
 export default App;
